a9fb2843aa64b6abb35ebeb16de83b9c0d761e6f
Ubuntu <ubuntu@ip-172-31-28-141.us-east-2.compute.internal>
2021-12-26 20:35:28 +0000

    refactor data interface

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -1,11 +1,8 @@
 package guru.springframework.sfgpetclinic.services;
 
-import java.util.Set;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService<Pet, Long> {
+
 }
