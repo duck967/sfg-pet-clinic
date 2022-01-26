a5f6ac304f1e6f0668d3aec739514d4ff4c07dd2
Ubuntu <ubuntu@ip-172-31-28-141.us-east-2.compute.internal>
2022-01-26 22:16:33 +0000

    Load Bootsrrap Data on Startup #14

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -3,6 +3,6 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 
-public interface PetService<Pet, Long> {
+public interface PetService extends CrudService<Pet, Long> {
 
 }
