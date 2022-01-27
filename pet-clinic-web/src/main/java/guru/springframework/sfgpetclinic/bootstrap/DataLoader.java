71e0a6390950f4521f80a69dd38df0cfae685270
Ubuntu <ubuntu@ip-172-31-28-141.us-east-2.compute.internal>
2022-01-27 21:42:55 +0000

    Update Map Impl to manage setting of ID Property #22

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -31,7 +31,6 @@ public class DataLoader implements CommandLineRunner{
     public void run(String... args) throws Exception {
         // TODO Auto-generated method stub
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
         // owner1.setAddress("123 Brickerel");
@@ -41,7 +40,6 @@ public class DataLoader implements CommandLineRunner{
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
         // owner2.setAddress("123 Brickerel");
@@ -54,7 +52,6 @@ public class DataLoader implements CommandLineRunner{
 
         
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
         // vet1.getSpecialities().add(savedRadiology);
@@ -62,7 +59,6 @@ public class DataLoader implements CommandLineRunner{
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
         // vet2.getSpecialities().add(savedSurgery);
